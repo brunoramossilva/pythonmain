075d15d5226388e1526d56ed9a7b57834bee3da6
brunoramossilva <brunogabriel0900@gmail.com>
2025-03-16 18:16:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from os import system
 animais = []
 
 def finalizar_programa():
-    resposta = (input('Deseja finalizar o programa? Y/N: '))
+
+    resposta = (input('Deseja finalizar o programa? (Y/N): '))
     if resposta.lower() == 'y':
         quit()
     elif resposta.lower() == 'n':
@@ -13,28 +14,43 @@ def finalizar_programa():
         print('Opção inválida')
         finalizar_programa()
 
+
 def print_inicial():
+
     system ('cls')
-    print ('-#' * 10)
-    print ('      PetCIn ')
-    print ('-#' * 10, end='\n\n')
+    print ('-#' * 11)
+
+    print ("""
+█▀█ █▀▀ ▀█▀ █▀▀ █ █▄░█
+█▀▀ ██▄ ░█░ █▄▄ █ █░▀█
+           """)
     
+    print ('-#' * 11, end='\n\n')
+
+
 def escolha():
+
     try:
         num = int(input('1. Cadastrar novo animal;\n'
         '2. Ver lista de animais cadastrados;\n'
         'Digite um número: '))
         return num
+    
     except ValueError:
+        system('cls')
         print('Opção Inválida!')
-        finalizar_programa()
+        escolha()
+
 
 def condicao (num):
+
     if num == 1:
+        system ('cls')
         animal = input('Digite o nome do animal: ')
         animais.append(animal)
         print(f'{animal} adicionado(a) com sucesso na lista!')
-        resposta = input ('Cadastrar outro animal? (Y/N): ')
+
+        resposta = input ('Deseja cadastrar outro animal? (Y/N): ')
         if resposta.lower() == 'y':
             system ('cls')
             condicao(num)
@@ -50,11 +66,13 @@ def condicao (num):
         input('Número inválido.\nDigite qualquer coisa para voltar ao menu principal: ')
         main()
 
+
 def main():
     system ('cls')
     print_inicial()
     num = escolha()
     condicao(num)
+
 
 if __name__ == '__main__':
     main()
