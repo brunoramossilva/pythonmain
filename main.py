67f16d98191dbb5cd1f1212be1aa9e88fe57677e
brunoramossilva <brunogabriel0900@gmail.com>
2025-03-16 17:14:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 from os import system
 
+def finalizar_programa():
+    resposta = (input('Deseja finalizar o programa? Y/N: '))
+    if resposta.lower() == 'y':
+        quit()
+    else:
+        print('Voltando ao menu principal...')
+        main()
+
 def print_inicial():
 
-
-    print("""
-        T
-        i
-        a
-        l
-        y""")
+    print("""T
+i
+a
+l
+y""")
     
 def escolha():
-    num = int(input('Escolha um número: '))
-    return num
+    try:
+        num = int(input('Escolha um número entre 1 e 2: '))
+        return num
+    except ValueError:
+        print('Opção Inválida!')
+        finalizar_programa()
 
 def condicao (num):
-    if num == 5:
+    if num == 1:
         print_inicial()
+        finalizar_programa()
 
-    else:
+    elif num == 2:
         system('cls')
         print('Você apagou tudo!')
+        finalizar_programa()
+    else:
+        input('Número inválido.\nDigite qualquer coisa para voltar ao menu principal: ')
+        main()
 
 def main():
+    system('cls')
     num = escolha()
     condicao(num)
 
